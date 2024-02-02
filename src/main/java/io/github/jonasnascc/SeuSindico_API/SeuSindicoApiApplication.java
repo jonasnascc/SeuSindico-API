4015542fb350878cf1dec711a8caff2ffc0417de
Jonas Nascimento <jonashnascimento@gmail.com>
2024-02-02 08:55:32 -0300

    project init

---
+++ src/main/java/io/github/jonasnascc/SeuSindico_API/SeuSindicoApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.jonasnascc.SeuSindico_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuSindicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuSindicoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jonasnascc/SeuSindico_API/SeuSindicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jonasnascc.SeuSindico_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuSindicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
